9eaf90663d3dc69db1ebcb1ef13c441f3fe8438e
Kristina <kuukristina@gmail.com>
2022-08-18 11:14:11 -0400

    HelloWorld.java is updated

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,5 +3,7 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("good all days");
+        System.out.println(" no doubt");
+
     }
 }
